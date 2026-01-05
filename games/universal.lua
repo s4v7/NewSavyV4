983b51353695b958602a06775297649b5f6d02d2
TheyFearSavy <theyfearsavy@gmail.com>
2026-01-05 23:39:04 +0500

    Update download URL for file retrieval

--- games/universal.lua
+++ games/universal.lua
@@ -14,7 +14,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/pxzw/savyv4/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/theyfearsavy-star/NewSavyV4/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
